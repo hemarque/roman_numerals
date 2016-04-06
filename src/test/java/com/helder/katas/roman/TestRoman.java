37f8776e1a6cfea5363cfb55cee337e9aadc4104
hemarque <hemarque@gmail.com>
2016-04-06 20:33:47 +0200

    test 1

--- src/test/java/com/helder/katas/roman/TestRoman.java
+++ src/test/java/com/helder/katas/roman/TestRoman.java
@@ -1,12 +1,18 @@
 package com.helder.katas.roman;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class TestRoman {
 
+	private Object intToRoman(int i) {
+		return "I";
+	}
+
 	@Test
 	public void testOne() {
-
+		assertEquals("Not expected roman numeral", "I", intToRoman(1));
 	}
 
 }
