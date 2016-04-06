749f337e802ec9b71427396a334f9f95e3b46bdf
hemarque <hemarque@gmail.com>
2016-04-06 21:09:29 +0200

    general algorithm

--- src/test/java/com/helder/katas/roman/TestRoman.java
+++ src/test/java/com/helder/katas/roman/TestRoman.java
@@ -7,8 +7,10 @@ import org.junit.Test;
 public class TestRoman {
 
 	private Object intToRoman(int decimal) {
-		final int[] baseDecimals = { 5, 4, 1 };
-		final String[] baseRomans = { "V", "IV", "I" };
+		final int[] baseDecimals = { 1000, 900, 500, 400, 100, 90, 50, 40, 10,
+				9, 5, 4, 1 };
+		final String[] baseRomans = { "M", "CM", "D", "CD", "C", "XC", "L",
+				"XL", "X", "IX", "V", "IV", "I" };
 		String roman = "";
 		for (int i = 0; i < baseDecimals.length; i++) {
 			while (decimal > (baseDecimals[i] - 1)) {
@@ -53,5 +55,15 @@ public class TestRoman {
 	@Test
 	public void testSeven() throws Exception {
 		assertEquals("Not expected roman numeral", "VII", intToRoman(7));
+	}
+
+	@Test
+	public void test1999() throws Exception {
+		assertEquals("Not expected roman numeral", "MCMXCIX", intToRoman(1999));
+	}
+
+	@Test
+	public void test2444() throws Exception {
+		assertEquals("Not expected roman numeral", "MMCDXLIV", intToRoman(2444));
 	}
 }
